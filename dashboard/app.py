1c960d24e77b8e4a4cbfb2c3a735811831310b85
Stephanie Farah <stephanie.grace.farah@gmail.com>
2026-03-14 02:00:35 -0400

    Fix dashboard to read submissions from raw table for deployment

--- dashboard/app.py
+++ dashboard/app.py
@@ -44,7 +44,7 @@ def load_data():
     """).df()
 
     submissions = con.execute("""
-        SELECT * FROM main_staging.stg_submissions
+        SELECT * FROM raw_submissions
     """).df()
 
     con.close()
